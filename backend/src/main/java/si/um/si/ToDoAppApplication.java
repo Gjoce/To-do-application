10b7576d0480009d71a10c98e07bae5e4a5b54aa
Georgi Dimov <gjorgji.dimov@student.um.si>
2024-10-22 17:35:45 +0200

    mysql env

--- backend/src/main/java/si/um/si/ToDoAppApplication.java
+++ backend/src/main/java/si/um/si/ToDoAppApplication.java
@@ -3,9 +3,9 @@ package si.um.si;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ToDoAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoAppApplication.class, args);
 	}
